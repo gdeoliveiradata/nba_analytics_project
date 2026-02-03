e99c74d2cdf67507b31af8b1873e90b36d0112fd
gdeoliveiradata <gdeoliveiradata@gmail.com>
2026-02-03 16:30:21 -0300

    Implementing the resource for NBA Players data, and merge loading on the PK.

--- src/01_bronze/ingestion.py
+++ src/01_bronze/ingestion.py
@@ -1,19 +1,25 @@
 import dlt
-from nba_api.stats.static import teams
+from nba_api.stats.static import teams, players
+
 
 @dlt.source
 def nba_data():
 
-    @dlt.resource()
+    @dlt.resource(primary_key="id", write_disposition="merge")
     def nba_teams():
         teams_list = teams.get_teams()
         yield from teams_list
 
-    return nba_teams
+    @dlt.resource(primary_key="id", write_disposition="merge")
+    def nba_players():
+        players_list = players.get_players()
+        yield from players_list
+
+    return nba_teams, nba_players
 
 # Set pipeline name, destination, and dataset name
-pl = dlt.pipeline(destination="duckdb")
+pl = dlt.pipeline(destination="duckdb", dataset_name="nba_dataset")
 
 # Run the pipeline with data and table name
-load_info = pl.run(nba_data, write_disposition="replace")
+load_info = pl.run(nba_data)
 print(load_info)
