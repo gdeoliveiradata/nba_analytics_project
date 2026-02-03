e07a686b74433e1ec77f81fdf19ff002d9a54c98
gdeoliveiradata <gdeoliveiradata@gmail.com>
2026-02-03 19:43:27 -0300

    Changed resource from all players to only active players; Implemented a transformer for players' headline stats.

--- src/01_bronze/ingestion.py
+++ src/01_bronze/ingestion.py
@@ -1,5 +1,7 @@
 import dlt
+import time
 from nba_api.stats.static import teams, players
+from nba_api.stats.endpoints import commonplayerinfo
 
 
 @dlt.source
@@ -11,15 +13,22 @@ def nba_data():
         yield from teams_list
 
     @dlt.resource(primary_key="id", write_disposition="merge")
-    def nba_players():
-        players_list = players.get_players()
+    def nba_active_players():
+        players_list = players.get_active_players()
         yield from players_list
 
-    return nba_teams, nba_players
+    @dlt.transformer(data_from=nba_active_players)
+    def nba_player_headline_stats(player):
+        player_id = player["id"]
+        time.sleep(0.6)
+        player_info = commonplayerinfo.CommonPlayerInfo(player_id=player_id).get_normalized_dict()["PlayerHeadlineStats"]
+        yield player_info
+
+    return nba_teams, nba_active_players, nba_player_headline_stats
 
 # Set pipeline name, destination, and dataset name
 pl = dlt.pipeline(destination="duckdb", dataset_name="nba_dataset")
 
 # Run the pipeline with data and table name
-load_info = pl.run(nba_data)
+load_info = pl.run(nba_data())
 print(load_info)
